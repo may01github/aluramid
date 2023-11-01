62b82ccb799eba75451f4957333dfe193ec095e3
may01github <135146321+may01github@users.noreply.github.com>
2023-11-01 08:16:32 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-function tocaSom(){
-    document.querySelector('#som_tecla_pom').play('');
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).
 }
 
 const listadeTeclas = document.querySelectorAll('.tecla')
 
+let contador = 0;
+
+while(contador < 9){
+
+const intrumento = listaDeTeclas[contador]
+
+const tecla = listaDeTeclas[contador];
+
+const idAudio = `#som_${instrumento};
+}
 
 
 
